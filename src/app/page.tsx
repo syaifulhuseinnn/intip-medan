309519d149eebde76a2a636131b03d44618b1210
syaifulhuseinnn <syaifulhuseinnn@gmail.com>
2024-06-23 22:27:20 +0700

    Fix: Next JS build failed because "ReferenceError: window is not defined"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
 "use client";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+
+import dynamic from "next/dynamic";
+
+const MapContainer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.MapContainer),
+  { ssr: false }
+);
+const TileLayer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.TileLayer),
+  { ssr: false }
+);
+const Marker = dynamic(
+  () => import("react-leaflet").then((mod) => mod.Marker),
+  { ssr: false }
+);
+const Popup = dynamic(() => import("react-leaflet").then((mod) => mod.Popup), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
