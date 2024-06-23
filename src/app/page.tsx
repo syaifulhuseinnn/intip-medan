19a73e66f12987b226f386d4176cf261037c779f
syaifulhuseinnn <syaifulhuseinnn@gmail.com>
2024-06-23 21:27:43 +0700

    Feature: Show a map view to visitor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+"use client";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 
 export default function Home() {
   return (
-    <main className="container h-screen w-full mx-auto">
-      <h1>Hello World</h1>
+    <main className="h-screen w-full mx-auto">
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+        className="h-screen w-full"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </main>
   );
 }
